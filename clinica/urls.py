54b9df9d02e1a009f13f72d2798630ffdc17d3a6
Vitorio <s.vitorio.alves@gmail.com>
2017-10-31 11:57:03 -0300

    Atualizando links e Agendar Consulta

--- clinica/urls.py
+++ clinica/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^generic$',views.generic),
     url(r'^index$',views.index),
-    url(r'^elements$',views.elements),
     url(r'^about$',views.about)
 ]
